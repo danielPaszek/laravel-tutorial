0a64258c19e542fd554d043c2e3e59e413153027
danielPaszek <daniel.paszek@student.put.poznan.pl>
2022-05-18 13:58:42 +0200

    before controllers

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get("/", function () {
+    return view("listings", [
+        "listings" => Listing::all(),
+    ]);
+});
+
+// Model binding
+Route::get("/listings/{listing}", function (Listing $listing) {
+    return view("listing", [
+        'listing' => $listing
+    ]);
+    
+});
+
